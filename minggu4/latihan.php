091b03ae73eee774de48f941389793abb24a95e3
unknown <roytd56@gmail.com>
2020-03-27 17:03:26 +0700

    perintah1

---
+++ minggu4/latihan.php
@@ -0,0 +1,11 @@
+<?php;
+//pungulangan
+//for
+
+for($i=0;$i<5;i++){
+	echo "Hello Word!<br>";
+}
+/* $i=0; bagian inisialisai/menentukan variabel
+$i<5; bagian kondisi/memberhentikan kondisi pengulangan ketika berjumlah <5
+i++ bagian increment(menambah) kondisi*/
+/>
