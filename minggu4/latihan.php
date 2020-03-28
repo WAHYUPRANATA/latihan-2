5147adfb10e37dfd6ff2183b1123b4c7c5a7cd18
unknown <roytd56@gmail.com>
2020-03-28 16:12:46 +0700

    task2

--- minggu4/latihan.php
+++ minggu4/latihan.php
@@ -2,8 +2,10 @@
 //pungulangan
 //for
 
+echo "Mengunakan perulangan FOR";
+
 for($i=0;$i<5;i++){
-	echo "Hello Word!<br>";
+	echo "Hai saya urutan ke-$i !<br>";
 }
 /* $i=0; bagian inisialisai/menentukan variabel
 $i<5; bagian kondisi/memberhentikan kondisi pengulangan ketika berjumlah <5
