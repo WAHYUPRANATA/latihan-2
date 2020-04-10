3e9e6a8396b21f54a31d9a850ee5c1766326e9b5
John Smith <roytd56@gmail.com>
2020-04-10 15:21:14 +0700

    task 1

---
+++ minggu6/date.php
@@ -0,0 +1,13 @@
+<?php
+//date untuk menampilkan tanggal dengan format tertentu
+
+    echo date("l");
+    echo " <br>";
+    echo date("d");
+    echo " <br>";
+    echo date("M");
+    echo " <br>";
+    echo date("m");
+    echo " <br>";
+    echo date("Y");
+?>
