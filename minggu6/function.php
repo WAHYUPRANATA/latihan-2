544ce52bbc07f326b2d8a2aa4594455053fbb4a1
John Smith <roytd56@gmail.com>
2020-04-10 17:13:34 +0700

    task 8

---
+++ minggu6/function.php
@@ -0,0 +1,15 @@
+<?php
+function perkalian($angka1, $angka2)
+{
+   $a= $angka1;
+   $b= $angka2;
+   $hasil= $a*$b;
+   return $hasil;
+}
+ 
+
+$hasil=perkalian(20,3);
+echo "Perkalian 20 x 3 adalah $hasil";
+echo "<br />";
+echo "Perkalian 9 x 2 adalah ".perkalian(9,2);
+?> 
