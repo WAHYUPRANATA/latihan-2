ace4324668a1375573b3a333c25cbe33a551d448
John Smith <roytd56@gmail.com>
2020-06-07 22:00:47 +0700

    login

---
+++ minggu9/login.php
@@ -0,0 +1,44 @@
+<?php
+//cek tombol submit
+if(isset($_POST["submit"])){
+//cek username dan password
+if($_POST["username"] == "admin" && $_POST["password"] == "123"){
+//jika benar,redirect ke halaman admin
+header("Location: admin.php");
+exit;
+}else
+//pesan error
+$eror = true;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+</head>
+<body>
+    <h1>Login Admin</h1>
+
+    <?php if (isset($eror)):?>
+    <p>salah satu gaes</p>
+    <?php endif;?>
+
+    <ul> 
+     <form action="" method="POST">
+        <li>
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username">
+        </li>
+        <li>
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+        </li>
+        <li>
+            <button type="submit" name="submit">Login</button>
+        </li>  
+    </ul>
+    </form>
+</body>
+</html>
